1d957e624279e8307f0bf00aa5299d079d6a5c35
Victor Hugo Zeschau <victorhugo@offertech.com.br>
2025-03-25 09:28:19 -0300

    ATT

---
+++ src/CaraCoroa/Main.java
@@ -0,0 +1,15 @@
+package CaraCoroa;
+
+public class Main {
+    public static void main(String[] args) {
+        CaraCoroa cc = new CaraCoroa();
+        ThreadCC cara = new ThreadCC("Cara", cc);
+        ThreadCC coroa = new ThreadCC("Coroa", cc);
+        try {
+            cara.t.join();
+            coroa.t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Notify/Message.java
@@ -0,0 +1,18 @@
+package Notify;
+
+public class Message {
+    private String msg;
+
+    public Message(String str) {
+        this.msg = str;
+    }
+
+    public String getMsg() {
+        return this.msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+}
